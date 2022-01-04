60b8c74d5a7eca74ebfd05952f55b0049397f497
Felipe <felipegarciaaffonso@gmail.com>
2022-01-04 15:02:24 -0500

    done

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,151 @@
 /*
-* This program ...
+* This program is a vehicle class.
 *
 * @author  Felipe Garcia Affonso
 * @version 1.0
-* @since   2021-12-04
+* @since   2021-12-25
 */
 
-"use strict"
+class Vehicle {
+  // this is a  class.
+  protected _speed: number
+  protected _maxSpeed: number
+  private _colour: string
 
-const ps = require("prompt-sync")
-let prompt = ps()
+  constructor (inicialColour: string, topspeed: number) {
+    this._colour = inicialColour
+    this._maxSpeed = topspeed
+    this._speed = 0
+  }
 
-function (s: someString) {
-    // Some text
-    return "some text" + someString
+  public get speed () {
+    return this._speed
+  }
+
+  public get maxSpeed () {
+    return this._maxSpeed
+  }
+
+  public get colour () {
+    return this._colour
+  }
+
+  public set speed (param: number) {
+    this._speed = param
+  }
+
+  public set colour (param: string) {
+    this._colour = param
+  }
+
+  public status () {
+    console.log('Speed: ' + this._speed + '\nMaxSpeed: ' + this._maxSpeed +
+                    '\nColour: ' + this._colour)
+  }
+
+  public accelerate (accPow: number, accTime: number) {
+    this._speed = (accPow * accTime) + this._speed
+    if (this._speed > this._maxSpeed) {
+      this._speed = this._maxSpeed
+    }
+  }
+
+  public break (breakPow: number, breakTime: number) {
+    this._speed = this._speed - (breakPow * breakTime)
+    if (this._speed < 0) {
+      this._speed = 0
+    }
+  }
 }
 
-var input = prompt("Enter input")
+class Bike extends Vehicle {
+  private _cadense: number
 
-console.log("\nDone.")
+  constructor (inicialColour: string, topspeed: number) {
+    super(inicialColour, topspeed)
+    this._cadense = 0
+  }
+
+  public get cadense () {
+    return this._cadense
+  }
+
+  public set cadense (param: number) {
+    this._cadense = param
+    this.speed = this._cadense * 2
+  }
+
+  public accelerate (accPow: number) {
+    this.cadense = this.cadense + accPow
+  }
+
+  public ringBell () {
+    console.log('\nDing ding!\n')
+  }
+
+  public status () {
+    super.status()
+    console.log('Cadense: ' + this._cadense)
+  }
+}
+
+class Truck extends Vehicle {
+  private _licensePlate: string
+
+  constructor (inicialColour: string, topspeed: number,
+    inicialPlate: string) {
+    super(inicialColour, topspeed)
+    this._licensePlate = inicialPlate
+  }
+
+  public get licensePlate () {
+    return this._licensePlate
+  }
+
+  public set licensePlate (param: string) {
+    this._licensePlate = param
+  }
+
+  public applyAir (airPressure: number) {
+    this.speed = this.speed - airPressure / 2
+  }
+
+  public status () {
+    super.status()
+    console.log('License plate: ' + this._licensePlate)
+  }
+}
+
+const bmx = new Bike('Red', 40)
+
+console.log('Created Bmx bike.\nStatus:\n')
+bmx.status()
+
+console.log('Set the cadense to 10\n')
+bmx.cadense = 10
+bmx.status()
+
+console.log('\nAccelerate by 15:')
+bmx.accelerate(15)
+bmx.status()
+
+console.log('\nRing bell.')
+
+const bigTruck = new Truck('Grey', 200, 'HGC-3456F')
+
+console.log('Created a Truck.\nStatus:\n')
+bigTruck.status()
+
+console.log('\nAccelerating, 10 of power for ten seconds:')
+bigTruck.accelerate(10, 10)
+console.log('New speed: ' + bigTruck.speed)
+
+console.log('\nBreaking, 10 of power for 10 sec.')
+bigTruck.accelerate(10, 10)
+console.log('New speed: ' + bigTruck.speed)
+
+console.log('\nApplyed air pressure of 10:')
+bigTruck.applyAir(10)
+console.log('New speed: ' + bigTruck.speed)
+
+console.log('\nDone.')
